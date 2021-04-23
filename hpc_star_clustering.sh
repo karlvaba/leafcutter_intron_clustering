8f4d7e0ea85d7a7a773aa6cde342d735152f148d
Karl <karl.vaba@gmail.com>
2021-04-23 15:33:37 +0300

    .

--- hpc_star_clustering.sh
+++ hpc_star_clustering.sh
@@ -5,4 +5,5 @@
  --gtf /gpfs/hpc/projects/genomic_references/annotations/eQTLCatalogue/v0.1/Homo_sapiens.GRCh38.96.gtf\
  --fasta /gpfs/hpc/projects/genomic_references/annotations/eQTLCatalogue/v0.1/Homo_sapiens.GRCh38.dna.primary_assembly.fa\
  --star_index /gpfs/hpc/projects/genomic_references/annotations/GRCh38/STAR_index_v90_oh100\
+ --aligner 'star'\
  -resume

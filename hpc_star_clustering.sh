8e9c5442017697db9a54e6c87ec964233775999d
Karl <karl.vaba@gmail.com>
2021-04-29 20:37:40 +0300

    Add vcf to star clustering script

--- hpc_star_clustering.sh
+++ hpc_star_clustering.sh
@@ -5,5 +5,6 @@
  --gtf /gpfs/hpc/projects/genomic_references/annotations/eQTLCatalogue/v0.1/Homo_sapiens.GRCh38.96.gtf\
  --fasta /gpfs/hpc/projects/genomic_references/annotations/eQTLCatalogue/v0.1/Homo_sapiens.GRCh38.dna.primary_assembly.fa\
  --star_index /gpfs/hpc/projects/genomic_references/annotations/GRCh38/STAR_index_v90_oh100\
+ --vcf /gpfs/hpc/projects/genomic_references/GEUVADIS/genotypes/GEUVADIS_GRCh38_filtered.vcf.gz\
  --aligner 'star'\
  -resume

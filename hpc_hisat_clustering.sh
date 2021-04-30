9d02c8e49e8369bd6de2a31bb36d20ad13eaafe9
Karl <karl.vaba@gmail.com>
2021-04-30 16:48:44 +0300

    Make hisat2 align unstranded

--- hpc_hisat_clustering.sh
+++ hpc_hisat_clustering.sh
@@ -1,7 +1,7 @@
 ../nextflow run ./main.nf\
  -profile tartu_hpc\
  --readPathsFile ./hpc_test_reads.tsv\
- --reverse_stranded\
+ --unstranded\
  --gtf /gpfs/hpc/projects/genomic_references/annotations/eQTLCatalogue/v0.1/Homo_sapiens.GRCh38.96.gtf\
  --hisat2_index /gpfs/hpc/projects/genomic_references/annotations/eQTLCatalogue/v0.1/hisat2_index_v96/Homo_sapiens.GRCh38.dna.primary_assembly\
  --aligner 'hisat'\

d0dd81b224e85162de482633a0e27acfe93e90b3
Karl <karl.vaba@gmail.com>
2021-05-03 13:46:20 +0300

    Create GEUVADIS reads file

--- hpc_hisat_clustering.sh
+++ hpc_hisat_clustering.sh
@@ -1,6 +1,6 @@
 ../nextflow run ./main.nf\
  -profile tartu_hpc\
- --readPathsFile ./hpc_test_reads.tsv\
+ --readPathsFile ./hpc_reads.tsv\
  --unstranded\
  --gtf /gpfs/hpc/projects/genomic_references/annotations/eQTLCatalogue/v0.1/Homo_sapiens.GRCh38.96.gtf\
  --hisat2_index /gpfs/hpc/projects/genomic_references/annotations/eQTLCatalogue/v0.1/hisat2_index_v96/Homo_sapiens.GRCh38.dna.primary_assembly\
--- hpc_star_wasp_clustering.sh
+++ hpc_star_wasp_clustering.sh
@@ -1,6 +1,6 @@
 ../nextflow run ./main.nf\
  -profile tartu_hpc\
- --readPathsFile ./hpc_test_reads.tsv\
+ --readPathsFile ./hpc_reads.tsv\
  --reverse_stranded\
  --gtf /gpfs/hpc/projects/genomic_references/annotations/eQTLCatalogue/v0.1/Homo_sapiens.GRCh38.96.gtf\
  --fasta /gpfs/hpc/projects/genomic_references/annotations/eQTLCatalogue/v0.1/Homo_sapiens.GRCh38.dna.primary_assembly.fa\
